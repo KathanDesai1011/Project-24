1e8614d0041944d92fc7113b2d026e878488a65d
KathanDesai1011 <77230545+KathanDesai1011@users.noreply.github.com>
2021-03-24 11:53:04 +0530

    Add files via upload

---
+++ Iron.js
@@ -0,0 +1,31 @@
+class Iron{
+	constructor(x,y,width,height){
+		
+		var options = {
+			restitution: 0.8,
+			friction: 3,
+			density: 30
+		}
+
+		this.body=Bodies.rectangle(x,y,width,height,options)
+        this.width = width;
+        this.height = height;
+		World.add(world, this.body);
+
+	}
+	
+	display(){
+
+		var pos=this.body.position;		
+		push()
+		translate(pos.x, pos.y);
+		rectMode(CENTER)
+		strokeWeight(1);
+		stroke("black");
+		fill("grey");
+		rect(0,0,this.width,this.height);
+		pop()
+		
+	}
+
+}
